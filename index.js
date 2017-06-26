43076a054bafa96152840a3b11489e5a7b9259ab
vishalmishra4u <mishravishal0912@gmail.com>
2017-06-26 23:54:16 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -3,63 +3,29 @@ module.exports = {
 };
 
 function generatePassword(level, size){
+  var passString = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&+-/*{}[]()%<>:;";
 
-passLevel5 = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&+-/*{}[]()%<>:;";
-
-  switch(level){
-    case 1 : return generateLevel1Pass(passLevel1,size);
-    case 2 : return generateLevel2Pass(passLevel2,size);
-    case 3 : return generateLevel3Pass(passLevel3,size);
-    case 4 : return generateLevel4Pass(passLevel4,size);
-    case 5 : return generateLevel5Pass(passLevel5,size);
+  return generatePass(passString,size);
   }
 }
 
 function generateLevel1Pass(passLevel1,size){
   var password = "";
 
+  if(){
+
+  }
+  else if(){
+
+  }
+  else if(){
+
+  }
+  else{
+    
+  }
   for(var i = 0; i < size; i++){
     password += passLevel1.charAt(Math.floor(Math.random() * passLevel1.length));
-  }
-  return password;
-}
-
-
-function generateLevel2Pass(passLevel2,size){
-  var password = "";
-
-  for(var i = 0; i < size; i++){
-    password += passLevel2.charAt(Math.floor(Math.random() * passLevel2.length));
-  }
-  return password;
-}
-
-
-function generateLevel3Pass(passLevel3,size){
-  var password = "";
-
-  for(var i = 0; i < size; i++){
-    password += passLevel3.charAt(Math.floor(Math.random() * passLevel3.length));
-  }
-  return password;
-}
-
-
-function generateLevel4Pass(passLevel4,size){
-  var password = "";
-
-  for(var i = 0; i < size; i++){
-    password += passLevel4.charAt(Math.floor(Math.random() * passLevel4.length));
-  }
-  return password;
-}
-
-
-function generateLevel5Pass(passLevel5,size){
-  var password = "";
-
-  for(var i = 0; i < size; i++){
-    password += passLevel5.charAt(Math.floor(Math.random() * passLevel5.length));
   }
   return password;
 }
